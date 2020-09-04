7b981e5bac2edbd0f03f2cd3caca0acd80bdb33b
brianadit24 <brian.adityaherman@gmail.com>
2020-09-04 09:34:12 +0700

    Cleaning Code and Assets

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -8,7 +8,7 @@ class AppBar extends HTMLElement {
         this.innerHTML = `
         <nav>
             <div class="nav-wrapper pink">
-            <img class="logo brand-logo center" src="src/images/cookey-logo.png" alt="cookey-logo">
+            <img class="logo brand-logo center" src="https://i.ibb.co/9cT190G/cookey-logo.png" alt="cookey-logo">
             </div>
         </nav>`;
     }
