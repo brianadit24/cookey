6698428adc890550012ecc4daac9aa1fc34e6588
brianadit24 <brian.adityaherman@gmail.com>
2020-09-04 09:02:11 +0700

    Reconfigure Package.json and Background Image

--- src/script/component/meal-item.js
+++ src/script/component/meal-item.js
@@ -7,22 +7,22 @@ class MealItem extends HTMLElement {
   
     render() {
         this.innerHTML = `
-            <div class="card">
-            <div class="card-image">
-                <img src="${this._meal.strMealThumb}" alt="Meal Image">
-                <a href="#" class="halfway-fab btn-floating pink pulse">
-                <i class="material-icons">favorite</i>
-                </a>
-            </div>
-            <div class="card-content">
-                <span class="card-title pink-text"><b>${this._meal.strMeal}</b></span>
-                <br>
-                <h6><b>Instructions :</b></h6>
-                <p>${this._meal.strInstructions}</p>
-            </div>
-            <div class="card-action center pink pulse">
-                <a href="${this._meal.strYoutube}" target="_blank" class="white-text">Check This Instruction Video</a>
-            </div>
+            <div class="card" id="id${this._meal.idMeal}">
+                <div class="card-image">
+                    <img src="${this._meal.strMealThumb}" alt="Meal Image">
+                    <a href="#id${this._meal.idMeal}" class="halfway-fab btn-floating pink pulse">
+                    <i class="material-icons">favorite</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <span class="card-title pink-text"><b>${this._meal.strMeal}</b></span>
+                    <br>
+                    <h6><b>Instructions :</b></h6>
+                    <p>${this._meal.strInstructions}</p>
+                </div>
+                <div class="card-action center pink pulse">
+                    <a href="${this._meal.strYoutube}" target="_blank" class="white-text">Check This Instruction Video</a>
+                </div>
             </div>`;
     }
 }
